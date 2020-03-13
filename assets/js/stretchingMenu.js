7185f3cadf5c3b89db35bd8694b423f4aa8bd4dc
Matteo Francia <matteo.francia@gmail.com>
2020-03-13 08:46:37 +0100

    Refactoring

--- assets/js/stretchingMenu.js
+++ assets/js/stretchingMenu.js
@@ -1,6 +1,6 @@
-function loadMenu() {
-    var titles = ["Standard - Every Day Stretch", "Cyclocross/MTB", "[5 min] Cyclocross/MTB", "Shoulder Stretch"];
-    var links = ["index.html", "mtb.html", "mtb_short.html", "shoulder.html"];
+$(document).ready(function () {
+    var titles = ["[9'] Standard", "[9'] Bike", "[5'] Bike Short", "[9'] Shoulder and Hips"];
+    var links = ["index.html", "bike.html", "bike_short.html", "shoulder_and_hips.html"];
     $('#stretchingMenu').empty();
     $.each(titles, function (i, t) {
             $('#stretchingMenu').append($(`
@@ -8,4 +8,4 @@ function loadMenu() {
             `).html(t).attr('href', links[i]));
             $('#stretchingMenu').append($('<div class="dropdown-divider"></div>'));
     });
-}
+});
